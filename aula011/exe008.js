6fb26a5749ff761912b6fc5f0413ae9bda4eb9bc
Igor Souza <100320787+souzaigor499@users.noreply.github.com>
2022-03-10 19:57:58 -0300

    att de exercícios

---
+++ aula011/exe008.js
@@ -0,0 +1,6 @@
+var vel = 15
+console.log(`A velocidade é ${vel} Km/h.`)
+if (vel > 60){
+    console.log("Você ultapassou o limite de velocidade. MULTADO!")
+}
+console.log("Dirija sempre utilizando o cinto de segurança!")
