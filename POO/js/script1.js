679369e674e95de06a44916068d87e2063370047
Igor Souza <souzaigor499@gmail.com>
2022-07-27 17:34:38 -0300

    POO

---
+++ POO/js/script1.js
@@ -0,0 +1,21 @@
+class Produto{
+
+    constructor() {
+        this.id = 0;
+        this.nomeProduto = '';
+        this.valor = 0;
+        
+    }
+
+    adicionar() {
+        alert('Vamos adicionar um produto')
+    }
+
+    excluir() {
+        alert('Item deletado')
+    }
+
+}
+
+var produto = new Produto()
+ 
