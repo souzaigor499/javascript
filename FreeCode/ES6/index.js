0a85ca46fcb00bc14dbfd2ab0263f4080a117e3a
Igor Souza <souzaigor499@gmail.com>
2023-05-25 14:27:11 -0300

    destructuring

--- FreeCode/ES6/index.js
+++ FreeCode/ES6/index.js
@@ -1,13 +1,35 @@
-let word1 = 'igor'
-let word2 = 'souza'
-let num1 = 1
-let num2 = 3 
+// TEMPLATE STRINGS
+// let word1 = 'igor'
+// let word2 = 'souza'
+// let num1 = 1
+// let num2 = 3 
 
-const fullName = `${num1 + num2} 
-${word2}`
+// const fullName = `${num1 + num2} 
+// ${word2}`
 
-let example = `${word1}
-${word2}
-`
-document.getElementById('example').innerText = example
-console.log(fullName)
+// let example = `${word1}
+// ${word2}
+// `
+// document.getElementById('example').innerText = example
+// console.log(fullName)
+
+
+// DESTRUCTURING OBJECTS
+
+// const personalInformation = {
+//     firstName: 'igor',
+//     lastName: 'souza',
+//     city: 'maua',
+//     state: 'sp',
+//     zipCode: '09321'
+// }
+
+// const {firstName: fn , lastName: ln} = personalInformation
+
+// console.log(`${fn} ${ln}`)
+
+// DESTRUCTURING ARRAYS
+
+// let [firstName, lastName, middleName] = ['igor', 'souza', 'soares']
+// lastName = 'santos'
+// console.log(lastName)
