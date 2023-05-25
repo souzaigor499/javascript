37dee6ab53144a643e2b2837bbff40b104a99a5d
Igor Souza <souzaigor499@gmail.com>
2023-05-25 14:15:04 -0300

    ES6 start

---
+++ FreeCode/ES6/index.js
@@ -0,0 +1,13 @@
+let word1 = 'igor'
+let word2 = 'souza'
+let num1 = 1
+let num2 = 3 
+
+const fullName = `${num1 + num2} 
+${word2}`
+
+let example = `${word1}
+${word2}
+`
+document.getElementById('example').innerText = example
+console.log(fullName)
