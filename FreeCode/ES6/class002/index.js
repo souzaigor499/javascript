acac6e08485e31a4ccb6d7252a110d4627000c8f
Igor Souza <souzaigor499@gmail.com>
2023-05-26 14:14:18 -0300

    padstart and padend

--- FreeCode/ES6/class002/index.js
+++ FreeCode/ES6/class002/index.js
@@ -38,15 +38,63 @@
 
 //ARROW FUNCTIONS
 
-function add(...nums){
-    let total = nums.reduce((x,y) => {
-        return x + y
-    })
-    console.log(total)
+// function add(...nums){
+//     let total = nums.reduce((x,y) => {
+//         return x + y
+//     })
+//     console.log(total)
 
-    let total1 = nums.reduce((x, y) => x + y)
-    console.log(total1)
+//     let total1 = nums.reduce((x, y) => x + y)
+//     console.log(total1)
 
-}
-add(4, 5, 6, 2, 9)
+// }
+// add(4, 5, 6, 2, 9)
+
+// DEFAULT PARAMS
+
+// function add(numArray = [1, 2]){
+//     let total = 0
+//     numArray.forEach((element) => {
+//         total += element
+//     });
+//     console.log(total)
+// }
+
+// add()
+
+//INCLUDES()
+
+// let numArray = [1,2,3,4,5]
+
+// function contem(value){
+//     console.log(numArray.includes(value))
+// }
+// contem(5)
+// LET AND CONST
+
+// if(false){
+//     let example = 5 // let escopo de bloco 
+// }
+
+// console.log(example)
+
+// const exemplo = {} // nao pode ter seu tipo de valor redefinido
+// exemplo.nome = 'igor'
+
+// console.log(exemplo)
+
+// IMPORT AND EXPORT 
+
+// import {data} from './example.js' // npm init -y {"type": "module"} Package.JSON
+
+// let updateData = data
+// console.log(updateData) 
+
+// padStart() padEnd()
+
+// let example = 'igor'
+
+// console.log(example.padStart(5, 'a'))
+
+
 
